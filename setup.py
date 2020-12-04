cebf730c050a0b5edffe762b3afddb25254bc8c1
Al-Baraa El-Hag <a.elhag01@gmail.com>
2020-12-03 21:56:25 -0500

    more commits

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+'''
+To run this you have to do install via `python3 setup.py install`
+'''
+
 from distutils.core import setup, Extension
 
 def main():
     setup(name="fputs",
           version="1.0.0",
           description="Python interface for the fputs C library function",
-          author="<your name>",
-          author_email="your_email@gmail.com",
+          author="Al-Baraa El-Hag",
+          author_email="a.elhag01@gmail.com",
           ext_modules=[Extension("fputs", ["fputsmodule.c"])])
 
 if __name__ == "__main__":
